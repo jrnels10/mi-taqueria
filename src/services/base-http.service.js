48ed3b839f34dbf861806d2d367ebf911abf534a
Jacob <jrnels10@gmail.com>
2021-04-18 11:46:29 -0700

    trying api url again

--- src/services/base-http.service.js
+++ src/services/base-http.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default class BaseHttpService {
-  BASE_URL = process.env.REACT_APP_API;
+  BASE_URL = 'https://mi-taqueria-api.herokuapp.com';
   _accessToken = null;
 
   constructor(props) {
